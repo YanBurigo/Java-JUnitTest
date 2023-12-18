4dd1dcdb0774533eb4a809e4863c6ff62cb17f54
Yan Burigo <yanburigoribeiro@gmail.com>
2023-12-18 18:10:54 -0300

    Create repeated test

--- src/test/java/io/github/yanburigo/math/SimpleMathAdvanceTest.java
+++ src/test/java/io/github/yanburigo/math/SimpleMathAdvanceTest.java
@@ -8,7 +8,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Test Math Operations in SimpleMath Class")
 class SimpleMathAdvanceTest {
@@ -21,7 +24,7 @@ class SimpleMathAdvanceTest {
 	}
 	
     @ParameterizedTest
-    @DisplayName("Test 6.2 / 2 = 3.1")
+    @DisplayName("Test MethodSource")
     //@MethodSource("testDivisionInputParameters")
     @MethodSource
     void testDivision(double firstNumber, double secondNumber, double expected) {
@@ -45,4 +48,46 @@ class SimpleMathAdvanceTest {
 			Arguments.of(18.3D, 3.1D, 5.90D)
 		);
 	}
+    
+    @ParameterizedTest
+    @DisplayName("Test File CSV")
+    @CsvSource({
+    	"6.2, 2, 3.1",
+    	"71, 14, 5.07",
+    	"18.3, 3.1, 5.90"
+    })
+    void csvTestDivision(double firstNumber, double secondNumber, double expected) {
+    	
+    	//Given
+        
+        //When
+        Double actual = math.division(firstNumber, secondNumber);
+        
+        //Then
+        assertEquals(expected, actual, 2D,
+                () -> firstNumber + "/" + secondNumber +
+                " did not produce " + expected + "!");
+    }
+    
+    @ParameterizedTest
+    @DisplayName("Test File CSV")
+    @CsvFileSource(resources = "/testDivision.csv")
+    void csvFileTestDivision(double firstNumber, double secondNumber, double expected) {
+    	
+    	//Given
+        
+        //When
+        Double actual = math.division(firstNumber, secondNumber);
+        
+        //Then
+        assertEquals(expected, actual, 2D,
+                () -> firstNumber + "/" + secondNumber +
+                " did not produce " + expected + "!");
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"Pelé", "Senna", "Keith Moon"})
+    void testValueSource(String firstName) {
+    	assertNotNull(firstName);
+    }
 }
