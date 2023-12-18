74c55cc42f6123195bec47bc6506793f795b01ac
Yan Burigo <yanburigoribeiro@gmail.com>
2023-12-18 10:30:40 -0300

    Adjust to BDD

--- src/test/java/io/github/yanburigo/math/SimpleMathTest.java
+++ src/test/java/io/github/yanburigo/math/SimpleMathTest.java
@@ -11,13 +11,17 @@ class SimpleMathTest {
     @Test
     @DisplayName("Test 6.2 + 2 = 8.2")
     void testSum_When_SixDotTwoIsAddedByTwo_ShouldReturnEightDotTwo() {
+    	
+    	//Given
         SimpleMath math = new SimpleMath();
         double firstNumber = 6.2D;
         double secondNumber = 2D;
-        
-        Double actual = math.sum(firstNumber, secondNumber);
         double expected = 8.2D;
         
+        //When
+        Double actual = math.sum(firstNumber, secondNumber);
+        
+        //Then
         assertEquals(expected, actual,
             () -> firstNumber + "+" + secondNumber +
             " did not produce " + expected + "!");
@@ -26,13 +30,17 @@ class SimpleMathTest {
     @Test
     @DisplayName("Test 6.2 - 2 = 4.2")
     void testSubtraction() {
+    	
+    	//Given
         SimpleMath math = new SimpleMath();
         double firstNumber = 6.2D;
         double secondNumber = 2D;
-        
-        Double actual = math.subtraction(firstNumber, secondNumber);
         double expected = 4.2D;
         
+        //When
+        Double actual = math.subtraction(firstNumber, secondNumber);
+        
+        //Then
         assertEquals(expected, actual,
                 () -> firstNumber + "-" + secondNumber +
                 " did not produce " + expected + "!");
@@ -41,13 +49,17 @@ class SimpleMathTest {
     @Test
     @DisplayName("Test 6.2 * 2 = 12.4")
     void testMultiplication() {
+    	
+    	//Given
         SimpleMath math = new SimpleMath();
         double firstNumber = 6.2D;
         double secondNumber = 2D;
-        
-        Double actual = math.multiplication(firstNumber, secondNumber);
         double expected = 12.4D;
         
+        //When
+        Double actual = math.multiplication(firstNumber, secondNumber);
+        
+        //Then
         assertEquals(expected, actual,
                 () -> firstNumber + "*" + secondNumber +
                 " did not produce " + expected + "!");
@@ -56,13 +68,17 @@ class SimpleMathTest {
     @Test
     @DisplayName("Test 6.2 / 2 = 3.1")
     void testDivision() {
+    	
+    	//Given
         SimpleMath math = new SimpleMath();
         double firstNumber = 6.2D;
         double secondNumber = 2D;
-        
-        Double actual = math.division(firstNumber, secondNumber);
         double expected = 3.1D;
         
+        //When
+        Double actual = math.division(firstNumber, secondNumber);
+        
+        //Then
         assertEquals(expected, actual,
                 () -> firstNumber + "/" + secondNumber +
                 " did not produce " + expected + "!");
@@ -77,13 +93,17 @@ class SimpleMathTest {
     @Test
     @DisplayName("Test (6.2 + 2) / 2 = 4.1")
     void testMean() {
+    	
+    	//Given
         SimpleMath math = new SimpleMath();
         double firstNumber = 6.2D;
         double secondNumber = 2D;
-        
-        Double actual = math.mean(firstNumber, secondNumber);
         double expected = 4.1D;
         
+        //When
+        Double actual = math.mean(firstNumber, secondNumber);
+        
+        //Then
         assertEquals(expected, actual,
             () -> "(" + firstNumber + "+" + secondNumber + ")/2" +
             " did not produce " + expected + "!");
@@ -92,12 +112,16 @@ class SimpleMathTest {
     @Test
     @DisplayName("Test Square Root of 81 = 9")
     void testSquareRoot() {
+    	
+    	//Given
         SimpleMath math = new SimpleMath();
         double number = 81D;
         double expected = 9D;
         
+        //When
         Double actual = math.squareRoot(number);
         
+        //Then
         assertEquals(expected, actual,
             () -> "Square Root of " + number +
             " did not produce " + expected + "!");
