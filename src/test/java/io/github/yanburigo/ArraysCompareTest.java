9810358c532f044a66d4b013439a3efec120d7be
Yan Burigo <yanburigoribeiro@gmail.com>
2023-12-18 11:25:22 -0300

    Implements array test and performance test

---
+++ src/test/java/io/github/yanburigo/ArraysCompareTest.java
@@ -0,0 +1,34 @@
+package io.github.yanburigo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+class ArraysCompareTest {
+
+	@Test
+	void testSort() {
+		int[] numbers = {25, 8, 21, 32, 3};
+		int[] expectedArray = {3, 8, 21, 25, 32};
+		
+		Arrays.sort(numbers);
+		
+		assertArrayEquals(numbers, expectedArray);
+	}
+	
+	@Test
+	//@Timeout(1)
+	@Timeout(value = 15, unit = TimeUnit.MILLISECONDS)
+	void testSortPerformance() {
+		int[] numbers = {25, 8, 21, 32, 3};
+		for(int i = 0; i < 100000000; i++) {
+			numbers[0] = i;
+			Arrays.sort(numbers);
+		}
+	}
+
+}
